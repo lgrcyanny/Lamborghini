00ab04ecc34b5ede6437db1707bfa045eca46eb7
lgrcyanny <lgrcyanny@gmail.com>
2014-03-22 17:54:27 +0800

    Finish the first version and upload to the competition

--- README.md
+++ README.md
@@ -2,3 +2,19 @@ Lamborghini
 ===========
 
 The recommendation algorithm for Ali Recommendation Algorithm Competition
+
+Installation
+=======
+1. Download Eclipse JavaEE
+2. git clone https://github.com/lgrcyanny/Lamborghini.git
+3. run maven install
+
+Algorithm
+========
+1. Calculate predict ratings for each user of each brand with MapReduce<BR>
+please run "RatingsGenerator.java" in eclipse
+2. Calculate repeat buy records with MapReduce<BR>
+please run "RepeatBuyGenerator.java"
+3. Calculate Recommendations with Collaborative Filtering User Based Algorithm
+4. Cominbe User Based results with repeat buy records to give the final reuslt<BR>
+please run "RecommendationsGenerator.java"
---
+++ src/main/java/com/lmb/recommendations/ItemBasedRecommender.java
@@ -0,0 +1,45 @@
+package com.lmb.recommendations;
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.recommender.CachingRecommender;
+import org.apache.mahout.cf.taste.impl.recommender.GenericItemBasedRecommender;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.recommender.SamplingCandidateItemsStrategy;
+import org.apache.mahout.cf.taste.impl.similarity.AveragingPreferenceInferrer;
+import org.apache.mahout.cf.taste.impl.similarity.LogLikelihoodSimilarity;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+import org.apache.mahout.cf.taste.impl.similarity.jdbc.MySQLJDBCInMemoryItemSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.CandidateItemsStrategy;
+import org.apache.mahout.cf.taste.recommender.MostSimilarItemsCandidateItemsStrategy;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.Recommender;
+import org.apache.mahout.cf.taste.similarity.ItemSimilarity;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ItemBasedRecommender {
+	private static final Logger log = LoggerFactory.getLogger(ItemBasedRecommender.class);
+	
+	public static void main(String[] args) throws Exception {
+		
+		DataModel model = new FileDataModel(new File("src/main/resources/part-r-00000"));
+		ItemSimilarity similarity = new PearsonCorrelationSimilarity(model);
+		Recommender recommender = new GenericItemBasedRecommender(model, similarity);		
+		CachingRecommender cachingRecommender = new CachingRecommender(recommender);
+		long start = System.currentTimeMillis();
+		List<RecommendedItem>  recommendations = cachingRecommender.recommend(4184250, 20);
+		long end = System.currentTimeMillis();
+		for (Iterator<RecommendedItem> iterator = recommendations.iterator(); iterator.hasNext(); ) {
+			RecommendedItem item = iterator.next();
+			System.out.println(item.getItemID() + "-" + item.getValue());
+		}	
+		log.info("time : " + (end -  start));
+	}
+}
